feca912c6bfbf5bd4af4bd00d4b23dc8ce87de59
Roman Shabanov <gettingold@mail.ru>
2017-06-28 12:31:46 +0300

    #25 eslint approved

--- src/js/live-reload.js
+++ src/js/live-reload.js
@@ -1,36 +1,32 @@
-if(window.location.href.indexOf('chrome') > -1){
-    var chokidar = require('chokidar');
-    var watcher  = chokidar.watch('.', {ignored: /[\/\\]\./});
-    var reloading = false;
+const chokidar = require('chokidar');
 
-    function triggerReload(){
+function triggerReload() {
+  console.warn('Reloading app...');
 
-        console.warn('Reloading app...');
+  if (location) {
+    location.reload();
+  }
+}
 
-        if(location){
-            location.reload();
-        }
+if (window.location.href.indexOf('chrome') > -1) {
+  const watcher = chokidar.watch('.', {
+    ignored: /[\/\\]\./,
+  });
+  let reloading = false;
 
+  watcher.on('all', (event, path) => {
+    if (event === 'change' && path && (path.indexOf('public/') > -1 || path.indexOf('src/') > -1)) {
+      if (path.indexOf('.css') > -1 || path.indexOf('.scss') > -1) {
+        const styles = document.querySelectorAll('link[rel=stylesheet]');
+
+        styles.forEach((style) => {
+          const restyled = `${style.getAttribute('href')}?v=${Math.floor((Math.random() * 10000) + 1)}`;
+          style.setAttribute('href', restyled);
+        });
+      } else if (!reloading) {
+        reloading = true;
+        setInterval(triggerReload, 100);
+      }
     }
-
-    watcher.on('all', function(event, path) {
-        if (event == "change" && path && (path.indexOf('public/')  > -1 || path.indexOf('src/') > -1)){
-
-            if(path.indexOf('.css') > -1 || path.indexOf('.scss') > -1){
-                var styles = document.querySelectorAll('link[rel=stylesheet]');
-
-                for (var i = 0; i < styles.length; i++) {
-                    // reload styles
-                    var restyled = styles[i].getAttribute('href') + '?v='+Math.random(0,10000);
-                    styles[i].setAttribute('href', restyled);
-                }
-            }else{
-                if(!reloading){
-                    reloading = true;
-                    setInterval(triggerReload, 100);
-                }
-            }
-        }
-
-    });
+  });
 }
--- util/version.js
+++ util/version.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 
-'use strict';
+const fs = require('fs');
+const shell = require('shelljs');
 
-var fs = require('fs');
-var shell = require('shelljs');
+const getCommitHash = () => {
+// exec git command to get the hash of the current commit
+// git rev-parse HEAD
 
-var getCommitHash = function() {
-	//exec git command to get the hash of the current commit
-	//git rev-parse HEAD
+  const hash = shell.exec('git rev-parse HEAD', {
+    silent: true,
+  }).output.trim().substr(0, 7);
 
-	var hash = shell.exec('git rev-parse HEAD', {
-		silent: true
-	}).output.trim().substr(0, 7);
-	if (hash === 'fatal: ')
-		return 'n/a';
-	return hash;
-}
+  return (hash === 'fatal: ' ? 'n/a' : hash);
+};
 
-var commitHash = getCommitHash();
-var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-console.log('v' + json.version + ' #' + commitHash);
+const commitHash = getCommitHash();
+const json = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+console.log(`v${json.version} #${commitHash}`);
 
-var content = 'window.version="' + json.version + '";';
-content = content + '\nwindow.commitHash="' + commitHash + '";';
-fs.writeFileSync("./src/js/version.js", content);
-
+const content = `window.version="${json.version}";\nwindow.commitHash="${commitHash}";`;
+fs.writeFileSync('./src/js/version.js', content);
