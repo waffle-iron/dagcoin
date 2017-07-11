fa7325bc25e560f004d6092fc50f4b714fd58b9a
lasha <lasha.tsintsabadze@ignite.ee>
2017-07-11 11:05:54 +0300

    lint fixes for application service

--- src/js/services/applicationService.js
+++ src/js/services/applicationService.js
@@ -1,30 +1,28 @@
-
+/* eslint-disable import/no-extraneous-dependencies,import/no-unresolved */
 angular.module('copayApp.services')
-  .factory('applicationService', ($rootScope, $timeout, isCordova, nodeWebkit, go) => {
-    const root = {};
+.factory('applicationService', ($rootScope, $timeout, isCordova, nodeWebkit, go) => {
+  const root = {};
 
-    root.restart = function () {
-      const hashIndex = window.location.href.indexOf('#/');
-      if (isCordova) {
-        window.location = window.location.href.substr(0, hashIndex);
-        $timeout(() => {
-          $rootScope.$digest();
-        }, 1);
-      } else {
-        // Go home reloading the application
-        if (nodeWebkit.isDefined()) {
-          go.walletHome();
-          $timeout(() => {
-            const win = require('nw.gui').Window.get();
-            win.reload(3);
-            // or
-            win.reloadDev();
-          }, 100);
-        } else {
-          window.location = window.location.href.substr(0, hashIndex);
-        }
-      }
-    };
+  root.restart = function () {
+    const hashIndex = window.location.href.indexOf('#/');
+    if (isCordova) {
+      window.location = window.location.href.substr(0, hashIndex);
+      $timeout(() => {
+        $rootScope.$digest();
+      }, 1);
+    } else if (nodeWebkit.isDefined()) {
+      // Go home reloading the application
+      go.walletHome();
+      $timeout(() => {
+        const win = require('nw.gui').Window.get();
+        win.reload(3);
+        // or
+        win.reloadDev();
+      }, 100);
+    } else {
+      window.location = window.location.href.substr(0, hashIndex);
+    }
+  };
 
-    return root;
-  });
+  return root;
+});
