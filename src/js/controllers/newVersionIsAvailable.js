454579a0afd4b540b306421e0529dc16c3dbf897
lasha <lasha.tsintsabadze@ignite.ee>
2017-07-17 15:19:52 +0300

    lint fixes for new version

--- src/js/controllers/newVersionIsAvailable.js
+++ src/js/controllers/newVersionIsAvailable.js
@@ -1,25 +1,25 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('copayApp.controllers').controller('newVersionIsAvailable', function($scope, $modalInstance, go, newVersion){
+  angular.module('copayApp.controllers').controller('newVersionIsAvailable', ($scope, $modalInstance, go, newVersion) => {
+    $scope.version = newVersion.version;
 
-	$scope.version = newVersion.version;
+    $scope.openDownloadLink = function () {
+      let link = '';
+      if (navigator && navigator.app) {
+        link = 'https://play.google.com/store/apps/details?id=org.dagcoin.wallet';
+        if (newVersion.version.match('t$')) {
+          link += '.testnet';
+        }
+      } else {
+        link = `https://github.com/dagcoin/dagcoin/releases/tag/v${newVersion.version}`;
+      }
+      go.openExternalLink(link);
+      $modalInstance.close('closed result');
+    };
 
-	$scope.openDownloadLink = function(){
-		var link = 'https://github.com/dagcoin/dagcoin/releases/tag/v' + newVersion.version;
-		// todo: comment out after we will have package on google play
-// 		if (navigator && navigator.app) {
-// 			link = 'https://play.google.com/store/apps/details?id=org.byteball.wallet';
-// 			if (newVersion.version.match('t$'))
-// 				link += '.testnet';
-// 		}
-// 		else {
-// 			link = 'https://github.com/byteball/byteball/releases/tag/v' + newVersion.version;
-// 		}
-		go.openExternalLink(link);
-		$modalInstance.close('closed result');
-	};
-
-	$scope.later = function(){
-		$modalInstance.close('closed result');
-	};
-});
+    $scope.later = function () {
+      $modalInstance.close('closed result');
+    };
+  });
+}());
