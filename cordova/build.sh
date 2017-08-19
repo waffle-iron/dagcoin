147bde3b99df0697291ac34f73a6c90ef96a22a6
lasha <lasha.tsintsabadze@ignite.ee>
2017-08-20 00:24:38 +0300

    hotfixes for android package

--- cordova/build.sh
+++ cordova/build.sh
@@ -56,7 +56,7 @@ echo "Project directory is $PROJECT"
 if [ ! -d $PROJECT ]; then
 	cd $BUILDDIR
 	echo "${OpenColor}${Green}* Creating project... ${CloseColor}"
-	cordova create ../../byteballbuilds/project-$1 org.dagcoin.wallet Dagcoin
+	cordova create ../../byteballbuilds/project-$1 org.dagcoin Dagcoin
 	checkOK
 
 	cd $PROJECT
