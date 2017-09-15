181c26993da7a07ef0e04b56489c4422b1feb453
Roman Shabanov <gettingold@mail.ru>
2017-09-15 13:54:22 +0300

    Coping the QR-code

--- src/js/controllers/walletHome.js
+++ src/js/controllers/walletHome.js
@@ -412,6 +412,12 @@
         } else if (nodeWebkit.isDefined()) {
           nodeWebkit.writeToClipboard(addr);
         }
+
+        $scope.tooltipCopiedShown = true;
+
+        $timeout(() => {
+          $scope.tooltipCopiedShown = false;
+        }, 1000);
       };
 
       this.shareAddress = function (addr) {
